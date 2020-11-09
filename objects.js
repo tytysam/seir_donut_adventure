820d6a64f3528ee70073ac795e00d57d865a508f
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-11-09 14:51:56 -0700

    created hampster class

---
+++ objects.js
@@ -0,0 +1,20 @@
+// ==================================
+// Classes & Donut Adventure
+// ==================================
+
+class Hampster {
+  constructor(owner, name, price) {
+    this.owner = "";
+    this.name = name;
+    this.price = 15;
+  }
+  wheelRun() {
+    console.log("squeak squeak");
+  }
+  eatFood() {
+    console.log("nibble nibble");
+  }
+  getPrice() {
+    return this.price;
+  }
+}
