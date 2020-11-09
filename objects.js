636e9bc3557f1f75fdecdf11c3e0781f79b3f061
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-11-09 15:02:02 -0700

    created person class

--- objects.js
+++ objects.js
@@ -2,8 +2,8 @@
 // Classes & Donut Adventure
 // ==================================
 
-class Hampster {
-  constructor(owner, name, price) {
+class Hamster {
+  constructor(name) {
     this.owner = "";
     this.name = name;
     this.price = 15;
@@ -16,5 +16,49 @@ class Hampster {
   }
   getPrice() {
     return this.price;
+  }
+}
+
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.age = 0;
+    this.height = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.hamsters = [];
+    this.bankAccount = 0;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  greet() {
+    console.log(`Hello! My name is ${this.name}.`);
+  }
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+  exercise() {
+    this.weight--;
+  }
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount += 10;
+  }
+  buyHamster(hamster) {
+    const newHamster = new Hamster(hamster);
+    this.hamsters.push(newHamster);
+    this.mood += 10;
+    this.bankAccount -= getPrice();
   }
 }
